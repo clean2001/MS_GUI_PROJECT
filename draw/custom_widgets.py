febe1938ef5a70367a3e1c21f2d9bf9b3eee6db8
kkang0 <whye8510@naver.com>
2023-03-12 18:40:52 +0900

    UDT: add parsing function in custom~.py

--- draw/custom_widgets.py
+++ draw/custom_widgets.py
@@ -4,6 +4,10 @@ from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 
+# sys.path.append(os.getcwd())
+module_path = os.path.abspath(os.getcwd() + '\\..')
+if module_path not in sys.path:
+    sys.path.append(module_path)
 
 cur_path = os.path.dirname(os.path.realpath(__file__))
 cur_path = cur_path.replace('\\', '/')
@@ -59,7 +63,7 @@ class Toolbar(QWidget):
         self.toolbar = parent.addToolBar('Open File')
         self.toolbar.addAction(openAction)
 
-    def openFile(self):
+    def openFile(self, parent):
         fname = QFileDialog.getOpenFileName(self, 'Open file', './')
 
         if fname[0]:
@@ -73,3 +77,67 @@ class Toolbar(QWidget):
                 self.dialog.textEdit.setText(data)
 
                 self.dialog.show()
+
+
+    def parsing(self, filename):
+        filename = 'toy.mgf'
+        # filename = 'MS_GUI_PROJECT/draw/toy.mgf'
+
+        with open(filename) as f:
+            lines = f.read().splitlines()
+
+        line_len = len(lines)
+
+        data = [' ' for i in range(5)]  # 작은 list
+        newlist = []
+        flag = True
+        idx = 0  # 큰 list의 index
+
+        while idx < line_len - 1:
+            x = []
+            y = []
+            while flag:
+                if lines[idx] == '':
+                    idx += 1
+                    if idx == line_len:
+                        flag = False
+
+                elif lines[idx] == 'BEGIN IONS':
+                    idx += 1
+                elif lines[idx][0] == 'T':
+                    split_str = lines[idx].split('=')
+                    data[0] = split_str[1]
+                    idx += 1
+                elif lines[idx][0] == 'C':
+                    split_str = lines[idx].split('=')
+                    data[1] = split_str[1]
+                    idx += 1
+                elif lines[idx][0] == 'P':
+                    split_str = lines[idx].split('=')
+                    data[2] = split_str[1]
+                    idx += 1
+                elif lines[idx][0] == 'S':
+                    split_str = lines[idx].split('=')
+                    if split_str[0] == 'SCANS':
+                        data[3] = split_str[1]
+                    if split_str[0] == 'SEQ':
+                        data[4] = split_str[1]
+                    idx += 1
+                elif lines[idx] == 'END IONS':
+                    idx += 1
+                    flag = False
+                else:
+                    split_xy = lines[idx].split(' ')
+                    x.append(split_xy[0])
+                    y.append(split_xy[1])
+
+                    # data.append(lines[idx])
+                    idx += 1
+
+            data.append(x)
+            data.append(y)
+            newlist.append(data)
+            data = [' ' for i in range(5)]  # 다시 빈 list로 만들어주기
+            flag = True
+
+        return  newlist
--- draw/parsing.py
+++ draw/parsing.py
@@ -1,4 +1,14 @@
+# title = []
+# charge = []
+# pep_mass = []
+# scans = []
+# seq = []
+#
+# seq_len = []    # 각 sequence의 길이를 저장하는 list
+# pep_info = []   # 각 peptide의 정보 [title, charge, pep_mass, scans, seq, x, y] 저장하는 list
+
 filename = 'toy.mgf'
+# filename = 'MS_GUI_PROJECT/draw/toy.mgf'
 
 with open(filename) as f:
     lines = f.read().splitlines()
@@ -11,6 +21,8 @@ flag = True
 idx = 0     # 큰 list의 index
 
 while idx < line_len - 1:
+    x = []
+    y = []
     while flag:
         if lines[idx] == '':
             idx += 1
@@ -22,45 +34,39 @@ while idx < line_len - 1:
         elif lines[idx][0] == 'T':
             split_str = lines[idx].split('=')
             data[0] = split_str[1]
-            # data.append(split_str[1])
             idx += 1
         elif lines[idx][0] == 'C':
             split_str = lines[idx].split('=')
             data[1] = split_str[1]
-            # data.append(split_str[1])
             idx += 1
         elif lines[idx][0] == 'P':
             split_str = lines[idx].split('=')
             data[2] = split_str[1]
-            # data.append(split_str[1])
             idx += 1
         elif lines[idx][0] == 'S':
             split_str = lines[idx].split('=')
             if split_str[0] == 'SCANS':
                 data[3] = split_str[1]
-                # data.append(split_str[1])
             if split_str[0] == 'SEQ':
                 data[4] = split_str[1]
-                # data.append(split_str[1])
-            # data.append(split_str[1])
             idx += 1
         elif lines[idx] == 'END IONS':
             idx += 1
             flag = False
         else:
-            data.append(lines[idx])
+            split_xy = lines[idx].split(' ')
+            x.append(split_xy[0])
+            y.append(split_xy[1])
+
+            # data.append(lines[idx])
             idx += 1
 
+    data.append(x)
+    data.append(y)
     newlist.append(data)
     data = [' ' for i in range(5)]   # 다시 빈 list로 만들어주기
     flag = True
 
-for i in newlist:
-    print(i)
-    print(len(i))
-
-
-
-
-
-
+def return_data():
+    print(newlist)
+    return newlist
