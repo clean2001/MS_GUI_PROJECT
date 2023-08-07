33d28c0ddcaf1d5221721857cd3a930c61082937
kkang0 <whye8510@naver.com>
2023-08-07 13:52:05 +0900

    UDT: mass error 그래프 사이에 끼우기

--- mass_error.py
+++ mass_error.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
-# import spectrum_utils.plot as sup
-# import spectrum_plot as sup
 import mass_plot as mp
 import spectrum_utils.spectrum as sus
 
-def mass_error_plot(spectrum_top):
+def mass_error_plot(spectrum_top, spectrum_bottom):
     ## spectrum_utils 라이브러리(mp)의 mass_errors 함수 적용
-    fig, ax = plt.subplots(figsize=(10.5, 4))
-    mp.mass_errors(spectrum_top, plot_unknown=False, ax=ax)
+    fig = mp.facet(
+        spec_top=spectrum_top,
+        spec_mass_errors=spectrum_top,
+        spec_bottom=spectrum_bottom,
+        mass_errors_kws={"plot_unknown": False},
+        height=7,
+        width=10.5,
+    )   
+    
+    # fig, ax = plt.subplots(figsize=(10.5, 4))
+    # mp.mass_errors(spectrum_top, plot_unknown=False, ax=ax)
     plt.show()
     # plt.close()
