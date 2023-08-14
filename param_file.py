0e6e1fd7aba2dfc70fb2e232a556784ddf471012
kkang0 <whye8510@naver.com>
2023-08-14 15:22:49 +0900

    ETC: foo.params 생성하는 함수만

--- param_file.py
+++ param_file.py
@@ -3,7 +3,7 @@ def make_parameter_file(
     query_file, target_lib_file, decoy_lib_file, pept_tol_value, 
     isotope_tol_value_min, isotope_tol_value_max, frag_tol_value):
     
-    query_file_paths = '\n'.join([f'Spectra= {query_file}' for query_file in query_file_list])
+    # query_file_paths = '\n'.join([f'Spectra= {query_file}' for query_file in query_file_list])
     
     params_content = f"""\
         # Spectra=[FILENAME]
@@ -34,21 +34,9 @@ def make_parameter_file(
         FragTolerance= {frag_tol_value}
         """
     
+    # index별로 파일 생성
     params_filename = f'foo{index}.params'
     
     with open(params_filename, 'w') as params_file:
         params_file.write(params_content)
     
-# query file 여러 개 적용 해 본 것
-query_file_list = ['./data/b1906_293T_proteinID_01A_QE3_122212.mgf', './data/toy.mgf']
-target_lib_file = './data/Target_predicted_lib.msp'
-decoy_lib_file = './data/revDecoy_predicted_lib.msp'
-pept_tol_value = 10
-isotope_tol_value_min = 0
-isotope_tol_value_max = 0
-frag_tol_value = 0.02
-
-# 함수 사용 예시(query 개수마다 foo{index:0부터}.params file 생성)
-for index, query_file in enumerate(query_file_list):
-    make_parameter_file(query_file, target_lib_file, decoy_lib_file, pept_tol_value,
-               isotope_tol_value_min, isotope_tol_value_max, frag_tol_value)
